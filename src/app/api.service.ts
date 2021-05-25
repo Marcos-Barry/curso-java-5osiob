397213460e6d2b23e59d0730e5acdb1a280f6780
Marcos-Barry <rosario.marcosv@gmail.com>
2021-05-25 14:43:38 -0300

    3 files modified

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,18 +1,39 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+interface Response {
+  id: number;
+  nome: string;
+  area: string;
+  aulas: string;
+}
+
 @Injectable()
 export class ApiService {
-
-cursosUrl = 'afternoon-ocean-97191.herokuapp.com/curso';
+  cursosUrl = 'afternoon-ocean-97191.herokuapp.com/curso';
 
   constructor(private http: HttpClient) {}
 
-  listarcursos(){
+  currentResponse: Response;
+  updateList: Array<Response> = [];
+
+  listar() {
+    this.http
+      .get<Response>('https://afternoon-ocean-97191.herokuapp.com/curso')
+      .subscribe(data => {
+        if (!this.currentResponse) {
+          this.updateList.push(data);
+        }
+        this.updateList.push(data);
+        this.currentResponse = data;
+      });
+  }
+
+  listarcursos() {
     return this.http.get<Array<any>>(this.cursosUrl);
   }
 
-  criarCurso(curso : any){
+  criarCurso(curso: any) {
     return this.http.post(this.cursosUrl, curso);
   }
 }
--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-cursos',
@@ -6,10 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cursos.component.css']
 })
 export class CursosComponent implements OnInit {
+  constructor(public ApiService: ApiService) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
